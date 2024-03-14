a5e1b279356655a61eb171b1ccddf5c0a428d859
S4bril <filip.informatyka@vp.pl>
2024-03-14 11:52:08 +0100

    Start Pracownia2

---
+++ Pracownia2/zad1_nanogram.py
@@ -0,0 +1,17 @@
+def from_last_list(lst, block_size):
+    pass
+
+def operations(ls, sizes):
+
+    before = 0
+    inside = 0
+    for size in sizes:
+        for i in range(x):
+            inside += ls[i]
+
+
+    # find block of y size
+
+
+if __name__ == "__main__":
+    pass
