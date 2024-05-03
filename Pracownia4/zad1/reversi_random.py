94a97b39676f32b9f8e31b2112c3f5c48777bb06
S4bril <95976169+S4bril@users.noreply.github.com>
2024-05-03 09:48:09 +0200

    Add zad1

---
+++ Pracownia4/zad1/reversi_random.py
@@ -0,0 +1,128 @@
+import random
+
+M = 8
+
+move_list = []
+
+directions = [ (0,1), (1,0), (-1,0), (0,-1), (1,1), (-1,-1), (1,-1), (-1,1) ]
+
+grid = [
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, 1, 0, None, None, None],
+    [None, None, None, 0, 1, None, None, None],
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None]
+]
+
+fields = set()
+
+for i in range(M):
+    for j in range(M):
+        if grid[i][j] is None:
+            fields.add((j, i))
+
+def reset_grid():
+    global grid, fields, move_list
+    move_list = []
+    grid = [
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, 1, 0, None, None, None],
+        [None, None, None, 0, 1, None, None, None],
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None]
+    ]
+    fields = set()
+    for i in range(M):
+        for j in range(M):
+            if grid[i][j] is None:
+                fields.add((j, i))
+
+def get(x,y):
+    if 0 <= x < M and 0 <= y < M:
+        return grid[y][x]
+    return None
+
+def can_beat(x, y, d, player):
+    dx,dy = d
+    x += dx
+    y += dy
+    cnt = 0
+    while get(x, y) == 1-player:
+        x += dx
+        y += dy
+        cnt += 1
+    return cnt > 0 and get(x, y) == player
+
+def moves(self, player):
+    res = []
+    for (x,y) in fields:
+        if any( self.can_beat(x,y, direction, player) for direction in directions):
+            res.append( (x,y) )
+    if not res:
+        return [None]
+    return res
+
+def result():
+    res = 0
+    for y in range(M):
+        for x in range(M):
+            b = grid[y][x]
+            if b == 0:
+                res -= 1
+            elif b == 1:
+                res += 1
+    return res
+
+def terminal(self):
+    if not fields:
+        return True
+    if len(self.move_list) < 2:
+        return False
+    return self.move_list[-1] == self.move_list[-2] == None
+
+def result():
+    res = 0
+    for y in range(M):
+        for x in range(M):
+            b = grid[y][x]
+            if b == 0:
+                res -= 1
+            elif b == 1:
+                res += 1
+    return res
+
+def do_move(move, player):
+    global grid, fields
+    move_list.append(move)
+
+    if move == None:
+        return
+
+    x, y = move
+    x0, y0 = move
+    grid[y][x] = player
+    fields -= set([move])
+    for dx, dy in directions:
+        x, y = x0, y0
+        to_beat = []
+        x += dx
+        y += dy
+        while get(x, y) == 1 - player:
+            to_beat.append((x, y))
+            x += dx
+            y += dy
+        if get(x, y) == player:
+            for (nx, ny) in to_beat:
+                grid[ny][nx] = player
+
+def reversi_random(player):
+    ms = moves(player)
+    if ms:
+        return random.choice(ms)
+    return [None]
