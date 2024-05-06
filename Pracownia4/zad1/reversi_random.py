0a5ccfcf0b8da0988f6195d3db2b7962d3f14807
S4bril <95976169+S4bril@users.noreply.github.com>
2024-05-06 11:46:17 +0200

    Add last_step_back function

--- Pracownia4/zad1/reversi_random.py
+++ Pracownia4/zad1/reversi_random.py
@@ -1,8 +1,12 @@
 import random
 import time
 import copy
+from tqdm import tqdm
+import math
 
 M = 8
+
+rev = []
 
 move_list = []
 
@@ -19,24 +23,52 @@ grid = [
     [None, None, None, None, None, None, None, None]
 ]
 
-weights = [[100, -20, 10, 5, 5, 10, -20, 100],
-           [-20, -50, -2, -2, -2, -2, -50, -20],
-           [10, -2, -1, -1, -1, -1, -2, 10],
-           [5, -2, -1, -1, -1, -1, -2, 5],
-           [5, -2, -1, -1, -1, -1, -2, 5],
-           [10, -2, -1, -1, -1, -1, -2, 10],
-           [-20, -50, -2, -2, -2, -2, -50, -20],
-           [100, -20, 10, 5, 5, 10, -20, 100]]
+weights = [
+    [ 4, -3,  2,  2,  2,  2, -3,  4],
+    [-3, -4, -1, -1, -1, -1, -4, -3],
+    [ 2, -1,  1,  0,  0,  1, -1,  2],
+    [ 2, -1,  0,  1,  1,  0, -1,  2],
+    [ 2, -1,  0,  1,  1,  0, -1,  2],
+    [ 2, -1,  1,  0,  0,  1, -1,  2],
+    [-3, -4, -1, -1, -1, -1, -4, -3],
+    [ 4, -3,  2,  2,  2,  2, -3,  4]
+]
 
 fields = set()
 
-for i in range(M):
-    for j in range(M):
-        if grid[i][j] is None:
-            fields.add((j, i))
+def last_step_back():
+    global grid, fields, move_list, rev
+
+    move = move_list[-1]
+    move_list = move_list[:-1]
+
+    r = rev[-1]
+    rev = rev[:-1]
+    if move:
+        fields.add(move)
+        grid[move[0]][move[1]] = None
+
+        for cell in r:
+            grid[cell[0]][cell[1]] = 1 - grid[cell[0]][cell[1]]
+
+def print_grid():
+    print("  ", end="")
+    for col_idx in range(len(grid[0])):
+        print(f"{col_idx:2}", end="")
+    print("\n   ---------------")
+    for row_idx, row in enumerate(grid):
+        print(f"{row_idx:2}|", end="")
+        for cell in row:
+            if cell is None:
+                print("X", end=" ")  # Print empty cell
+            else:
+                print(cell, end=" ")  # Print cell value
+        print()
+    print()
 
 def reset_game():
-    global grid, fields, move_list
+    global grid, fields, move_list, rev
+    rev = []
     move_list = []
     grid = [
         [None, None, None, None, None, None, None, None],
@@ -52,11 +84,11 @@ def reset_game():
     for i in range(M):
         for j in range(M):
             if grid[i][j] is None:
-                fields.add((j, i))
+                fields.add((i, j))
 
 def get(x,y):
     if 0 <= x < M and 0 <= y < M:
-        return grid[y][x]
+        return grid[x][y]
     return None
 
 def can_beat(x, y, d, player):
@@ -72,6 +104,7 @@ def can_beat(x, y, d, player):
 
 def moves(player):
     res = []
+    # print(fields)
     for (x, y) in fields:
         if any(can_beat(x, y, direction, player) for direction in directions):
             res.append((x, y))
@@ -81,9 +114,9 @@ def moves(player):
 
 def result():
     res = 0
-    for y in range(M):
-        for x in range(M):
-            b = grid[y][x]
+    for x in range(M):
+        for y in range(M):
+            b = grid[x][y]
             if b == 0:
                 res -= 1
             elif b == 1:
@@ -97,28 +130,20 @@ def terminal():
         return False
     return move_list[-1] == move_list[-2] == None
 
-def result():
-    res = 0
-    for y in range(M):
-        for x in range(M):
-            b = grid[y][x]
-            if b == 0:
-                res -= 1
-            elif b == 1:
-                res += 1
-    return res
-
 def do_move(move, player):
-    global grid, fields
+    global grid, fields, rev
     move_list.append(move)
 
     if move == None:
+        rev.append(None)
         return
 
+    # print(move)
     x, y = move
     x0, y0 = move
-    grid[y][x] = player
+    grid[x][y] = player # y x ????
     fields -= set([move])
+    reversed_cells = []
     for dx, dy in directions:
         x, y = x0, y0
         to_beat = []
@@ -130,7 +155,9 @@ def do_move(move, player):
             y += dy
         if get(x, y) == player:
             for (nx, ny) in to_beat:
-                grid[ny][nx] = player
+                grid[nx][ny] = player
+                reversed_cells.append((nx, ny))
+    rev.append(reversed_cells)
 
 def heuristics(player):
     res = 0
@@ -168,25 +195,80 @@ def my_agent_heuristics(player):
             grid_copy = copy.deepcopy(grid)
             do_move((mx, my), player)
             h = heuristics(player)
-            # if player == 0:
-            #     h -= result()
-            # else:
-            #     h += result()
-            grid = grid_copy
+            if player == 0:
+                h -= result()
+            else:
+                h += result()
+            last_step_back(grid_copy)
             if h > best:
                 best = h
                 best_move = (mx, my)
         return best_move
     return None
 
-def my_agent_minmax(player):
-    def minimax()
+def my_agent_minmax(agent):
+    # if len(move_list) < 20:
+    #     # print("lol")
+    #     return random_move(agent)
+    def minimax(depth, maximizingPlayer, player):
+        global grid
+
+        if depth == 0 or terminal():
+            # if maximizingPlayer:
+            #     return None, heuristics(player)
+            # else:
+            #     return None, -heuristics(player)
+            if agent == 1:
+                return None, result()
+            else:
+                return None, -result()
+
+        ms = moves(player)
+
+        if not ms:
+            do_move(None, player)
+            return minimax(depth-1, not maximizingPlayer, 1 - player)
+            last_step_back()
+
+        if maximizingPlayer:
+            maxEval = -1000
+            bestMove = None
+            for (mx, my) in ms:
+                do_move((mx, my), player)
+                eval = minimax(depth-1, False, 1 - player)
+                if eval[1] > maxEval:
+                    maxEval = eval[1]
+                    bestMove = (mx, my)
+                last_step_back()
+            return bestMove, maxEval
+
+        else:
+            # ms = ms[:len(ms)//5]
+            minEval = 1000
+            bestMove = None
+            for (mx, my) in ms:
+                do_move((mx, my), player)
+                eval = minimax(depth-1, True, 1 - player)
+                if eval[1] < minEval:
+                    minEval = eval[1]
+                    bestMove = (mx, my)
+                last_step_back()
+            return bestMove, minEval
+
+
+            # mv = random.choice(ms)
+            # do_move(mv, player)
+            # eval = minimax(depth - 1, True, 1 - player)
+            # last_step_back()
+            # return mv, eval[1]
+
+    return minimax(200, True, agent)[0]
 
 def main():
     games = 1000
     my_agent_loses = 0
-    agents = [random_move, my_agent_heuristics]
-    for i in range(games):
+    agents = [random_move, my_agent_minmax]
+    for i in tqdm(range(games), desc="Simulating games"):
         player = 0
         if i < games / 2:
             agent = 0  # random agent is starting
@@ -195,6 +277,7 @@ def main():
         reset_game()
 
         while True:
+            # print(rev)
             m = agents[agent](player)
             do_move(m, player)
             player = 1 - player
